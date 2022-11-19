bc3ac52597cba199c0592b8a4f13e864e2ab2efb
Dionysus <dionysus-oss@proton.me>
2022-11-19 03:56:15 +0000

    Improve edge case handling for rcat UDP

--- rcat/src/udp.rs
+++ rcat/src/udp.rs
@@ -1,19 +1,14 @@
-use std::io;
-use tokio::io::{stdout, stdin};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Mutex;
+use tokio::io::{stdin, stdout};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::UdpSocket;
 use tokio::select;
-use tokio::sync::mpsc;
-use crate::common::read_write;
 
 pub async fn udp_connect(host: &String, port: &u16, listen_port: u16) -> Result<(), String> {
     let addr = format!("0.0.0.0:{}", listen_port);
-    let socket = UdpSocket::bind(addr).await.map_err(|_| "failed to bind return port")?;
+    let socket = UdpSocket::bind(addr).await.map_err(|_| "failed to bind to return port")?;
 
     let remote_addr = format!("{}:{}", host, port);
-    socket.connect(remote_addr).await.unwrap();
+    socket.connect(remote_addr).await.map_err(|_| "failed to connect")?;
 
     let mut stdin_buf = [0; 512];
     let mut network_in_buf = [0; 512];
@@ -25,31 +20,28 @@ pub async fn udp_connect(host: &String, port: &u16, listen_port: u16) -> Result<
     while active {
         select! {
             res = stdin.read(&mut stdin_buf) => {
-                if let Ok(amt) = res {
-                    if amt != 0 {
-                        socket.send(&stdin_buf[0..amt]).await.map_err(|_| "failed to write to stdout").unwrap();
+                if let Ok(amount) = res {
+                    if amount != 0 {
+                        socket.send(&stdin_buf[0..amount]).await.map_err(|_| "failed to write to the socket").unwrap();
+                    } else {
+                        active = false;
                     }
                 } else {
                     res.unwrap();
                 }
             }
             res = socket.recv(&mut network_in_buf) => {
-                 if let Ok(amt) = res {
-                    if amt != 0 {
-                        stdout().write(&network_in_buf[0..amt]).await.map_err(|_| "failed to write to stdout").unwrap();
+                if let Ok(amount) = res {
+                    // Note there is no associated else because a zero length UDP message is valid. Just skip writing
+                    if amount != 0 {
+                        stdout().write(&network_in_buf[0..amount]).await.map_err(|_| "failed to write to stdout").unwrap();
                     }
                 } else {
                     res.unwrap();
                 }
             }
-            _ = tokio::signal::ctrl_c() => {
-                println!("received terminate signal");
-                active = false;
-            }
         }
     }
-
-    println!("UDP connect finished");
 
     Ok(())
 }
@@ -69,19 +61,22 @@ pub async fn udp_serve(bind_host: &String, port: &u16) -> Result<(), String> {
     let mut active = true;
 
     while active {
-        select! {
+        select!(
             res = socket.recv_from(&mut network_in_buf) => {
-                 if let Ok((amt, remote_addr)) = res {
-                    if amt != 0 {
-                        // if is_connected {
-                        //     // Reject connections from other clients
-                        // }
+                if let Ok((amount, remote_addr)) = res {
+                    // Note there is no associated else because a zero length UDP message is valid. Just skip writing
+                    if amount != 0 {
+                        stdout().write(&network_in_buf[0..amount]).await.map_err(|_| "failed to write to stdout").unwrap();
+                    }
 
-                        stdout().write(&network_in_buf[0..amt]).await.map_err(|_| "failed to write to stdout").unwrap();
+                    if !is_connected {
+                        socket.connect(remote_addr).await.unwrap();
+                        is_connected = true;
 
-                        if !is_connected {
-                            socket.connect(remote_addr).await.unwrap();
-                            is_connected = true;
+                        // Output pending data from stdin
+                        if stdin_tmp_buf.len() != 0 {
+                            socket.send(stdin_tmp_buf.as_ref()).await.map_err(|_| "failed to write to the socket").unwrap();
+                            stdin_tmp_buf.clear();
                         }
                     }
                 } else {
@@ -89,31 +84,22 @@ pub async fn udp_serve(bind_host: &String, port: &u16) -> Result<(), String> {
                 }
             }
             res = stdin.read(&mut stdin_buf) => {
-                if let Ok(amt) = res {
-                    if amt != 0 {
+                if let Ok(amount) = res {
+                    if amount != 0 {
                         if is_connected {
-                            if stdin_tmp_buf.len() != 0 {
-                                socket.send(stdin_tmp_buf.as_ref()).await.map_err(|_| "failed to write to stdout").unwrap();
-                                stdin_tmp_buf.clear();
-                            }
-
-                            socket.send(&stdin_buf[0..amt]).await.map_err(|_| "failed to write to stdout").unwrap();
+                            socket.send(&stdin_buf[0..amount]).await.map_err(|_| "failed to write to the socket").unwrap();
                         } else {
-                            stdin_tmp_buf.extend_from_slice(&stdin_buf[0..amt]);
+                            stdin_tmp_buf.extend_from_slice(&stdin_buf[0..amount]);
                         }
+                    } else {
+                        active = false;
                     }
                 } else {
                     res.unwrap();
                 }
             }
-            _ = tokio::signal::ctrl_c() => {
-                println!("received terminate signal");
-                active = false
-            }
-        }
+        )
     }
-
-    println!("UDP serve finished");
 
     Ok(())
 }
